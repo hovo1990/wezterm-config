7781bc36720df1327e5379b027d3fd03e04c0685
Hovakim Grabski <hovakim.grabski@gmail.com>
2025-09-13 10:17:01 +0400

    trying to add resurrect v3

---
+++ config/resurrect_backup.lua
@@ -0,0 +1,89 @@
+local resurrectio = {
+
+    local wezterm = require("wezterm")
+    local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
+
+
+    resurrect.state_manager.periodic_save({
+        interval_seconds = 15 * 60,
+        save_workspaces = true,
+        save_windows = true,
+        save_tabs = true,
+    })
+
+    resurrect.state_manager.set_encryption({
+        enable = false,
+        private_key = wezterm.home_dir .. "/.age/resurrect.txt",
+        public_key = "age17yk5rw6vf4wrgwxgn69mpqemc2m7zy8taqdfgeyljqhnavl8dynszgk2s8",
+    })
+
+    wezterm.on("resurrect.error", function(err)
+        wezterm.log_error("ERROR!")
+        wezterm.gui.gui_windows()[1]:toast_notification("resurrect", err, nil, 3000)
+    end)
+
+    local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
+    workspace_switcher.apply_to_config({})
+
+    workspace_switcher.workspace_formatter = function(label)
+        return wezterm.format({
+            { Attribute = { Italic = true } },
+            { Foreground = { Color = colors.colors.ansi[3] } },
+            { Background = { Color = colors.colors.background } },
+            { Text = "󱂬 : " .. label },
+        })
+    end
+
+    wezterm.on("smart_workspace_switcher.workspace_switcher.created", function(window, path, label)
+        window:gui_window():set_right_status(wezterm.format({
+            { Attribute = { Intensity = "Bold" } },
+            { Foreground = { Color = colors.colors.ansi[5] } },
+            { Text = basename(path) .. "  " },
+        }))
+        local workspace_state = resurrect.workspace_state
+
+        workspace_state.restore_workspace(resurrect.state_manager.load_state(label, "workspace"), {
+            window = window,
+            relative = true,
+            restore_text = true,
+
+            resize_window = false,
+            on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+        })
+    end)
+
+    wezterm.on("smart_workspace_switcher.workspace_switcher.chosen", function(window, path, label)
+        wezterm.log_info(window)
+        window:gui_window():set_right_status(wezterm.format({
+            { Attribute = { Intensity = "Bold" } },
+            { Foreground = { Color = colors.colors.ansi[5] } },
+            { Text = basename(path) .. "  " },
+        }))
+    end)
+
+    wezterm.on("smart_workspace_switcher.workspace_switcher.selected", function(window, path, label)
+        wezterm.log_info(window)
+        local workspace_state = resurrect.workspace_state
+        resurrect.state_manager.save_state(workspace_state.get_workspace_state())
+        resurrect.state_manager.write_current_state(label, "workspace")
+    end)
+
+    wezterm.on("smart_workspace_switcher.workspace_switcher.start", function(window, _)
+        wezterm.log_info(window)
+    end)
+    wezterm.on("smart_workspace_switcher.workspace_switcher.canceled", function(window, _)
+        wezterm.log_info(window)
+    end)
+
+    local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
+    smart_splits.apply_to_config(config, {
+        direction_keys = { "h", "j", "k", "l" },
+        modifiers = {
+            move = "CTRL",
+            resize = "ALT",
+        },
+    })
+
+}
+
+return resurrectio
